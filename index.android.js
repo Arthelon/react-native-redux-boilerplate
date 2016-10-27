9da723e999b44b37d0d178754c88768e3b9da287
Arthelon <hsing.daniel@gmail.com>
2016-10-27 09:22:42 +0800

    added shoutem to deps

--- index.android.js
+++ index.android.js
@@ -1,13 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
 } from 'react-native';
 import App from './app/App'
 
-AppRegistry.registerComponent('qrCode', () => App);
+AppRegistry.registerComponent('cameraClone', () => App);
